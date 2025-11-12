a8d552d186a82de7ba21a677e739696be2488777
Bohdan Klymov <programmer.b.k.339@gmail.com>
2025-11-12 07:39:03 +0100

    Add files via upload
    Updated the read_the_file.py file from week 4 to improve the syntax in the code.

--- Week4/read_the_file.py
+++ Week4/read_the_file.py
@@ -2,23 +2,14 @@
 # There is exactly one number per line. 
 # Read the file and output the three biggest numbers
 
+numbers = []
+
 with open("numbers.txt", "r") as file:
-    nums_list = []
     for line in file:
-        line = line.strip()
-        nums_list.append(line)
-    
-    max_number1 = max(nums_list)
-    
-    print(max_number1)
-    nums_list.remove(max_number1)
-    
-    max_number2 = max(nums_list)
-    
-    print(max_number2)
-    nums_list.remove(max_number2)
-    
-    max_number3 = max(nums_list)
-        
-    print(max_number3)
-    nums_list.remove(max_number3)
+        number = int(line.strip())
+        numbers.append(number)
+
+numbers.sort(reverse=True)
+
+for num in numbers[:3]:
+    print(num)
